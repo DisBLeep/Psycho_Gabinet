8bd82c2e907b877e96ce8734fcb3c779856bb880
DisBLeep <102161796+DisBLeep@users.noreply.github.com>
2023-01-18 19:25:31 +0100

    neu djangou

--- DjangoVer/playground/urls.py
+++ DjangoVer/playground/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 # Url Conf
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('panel/', views.panel),
+    path('test/', views.test)
 ]
--- DjangoVer/playground/views.py
+++ DjangoVer/playground/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts   import render
 from django.http        import HttpResponse
+from django.template       import loader
+
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'p_logn.html')
+    return HttpResponse("Hi")
+
+def panel(request):
+  template = loader.get_template('panel.html')
+  context = {
+    'testx': 'Linus',
+  }
+  return HttpResponse(template.render(context, request))
+
+def test(request):
+    return render(request, 'newtest.html')
